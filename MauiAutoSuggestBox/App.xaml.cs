0a308c34a963d4c22495d110533b07e6606cc04e
MartyIX <203266+MartyIX@users.noreply.github.com>
2023-10-17 14:44:50 +0200

    New MAUI project

---
+++ MauiAutoSuggestBox/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAutoSuggestBox
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
